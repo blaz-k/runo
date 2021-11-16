c915fa5b419ac3900ef1878cfb235037c9217cc7
Blaz <blazyy@gmail.com>
2021-11-16 11:59:30 +0100

    added global.css, navbar and footer component

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,19 @@
 <template>
-    <main>
-      <Navbar/>
-          <router-link to="/" class="navbar-brand">runo</router-link>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <router-link to="/" 
-                  >Home</router-link
-                >
-              </li>
-              <li class="nav-item">
-                <router-link to="/about">about</router-link>
-              </li>
-            </ul>
-          </div>
-      <router-view />
-    </main>
+  <main class="container">
+    <Navbar />
+    <router-view />
+  </main>
 </template>
 
 <script>
-
-import Navbar from "./components/Navbar.vue"
+import Navbar from "./components/Navbar.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Navbar
-}
-}
+    Navbar,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="footer-section">
+    <footer class="row py-5 my-5 border-top">
+      <div class="col">
+        <a
+          href="/"
+          class="d-flex align-items-center mb-3 link-dark text-decoration-none"
+        >
+          <h1>Contact the Publisher</h1>
+        </a>
+        <div>
+          <p><i></i>mike@runo.com</p>
+          <p><i> </i> +944 450 904 505</p>
+        </div>
+        <p class="copyright">Copyright @ 2022 Lugar Inc.</p>
+      </div>
+
+      <div class="col"></div>
+
+      <div class="col">
+        <h5>Explorate</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">About</a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">Partners</a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">Job Opportunities</a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">Advertise</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col">
+        <h5>Headquarter</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">Terms</a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">Conditions</a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">Policy</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col">
+        <h5>Connections</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">Facebook</a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">Twitter</a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">YouTube</a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">LinkedIn</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style></style>
