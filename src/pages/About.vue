7f4122359213ab4e448fc4c496781e0ddf0a1100
Blaz <blazyy@gmail.com>
2021-11-17 14:10:03 +0100

    corrected editors-pick; minor changes in others 

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,39 +1,82 @@
 <template>
   <div>
-    Seamlessly syndicate cutting-edge architectures rather than collaborative
-    collaboration and idea-sharing. Proactively incubate visionary interfaces
-    whereas premium benefits. Seamlessly negotiate ubiquitous leadership skills
-    rather than parallel ideas. Dramatically visualize superior interfaces for
-    best-of-breed alignments. Synergistically formulate performance based users
-    through customized relationships. Interactively deliver cross-platform ROI
-    via granular systems. Intrinsicly enhance effective initiatives vis-a-vis
-    orthogonal outsourcing. Rapidiously monetize market-driven opportunities
-    with multifunctional users. Collaboratively enhance visionary opportunities
-    through revolutionary schemas. Progressively network just in time customer
-    service without real-time scenarios. Synergistically drive e-business
-    leadership with unique synergy. Compellingly seize market positioning ROI
-    and bricks-and-clicks e-markets. Proactively myocardinate timely platforms
-    through distributed ideas. Professionally optimize enabled core competencies
-    for leading-edge sources. Professionally enhance stand-alone leadership with
-    innovative synergy. Rapidiously generate backend experiences vis-a-vis
-    long-term high-impact relationships. Authoritatively supply market-driven
-    mindshare and bricks-and-clicks opportunities. Holisticly create diverse
-    innovation through adaptive communities. Efficiently empower seamless
-    meta-services with impactful opportunities. Distinctively transition virtual
-    outsourcing with focused e-tailers. “ Monotonectally seize superior
-    mindshare rather than efficient technology. ” Compellingly enhance seamless
-    resources through competitive content. Continually actualize 24/365
-    alignments for resource-leveling platforms. Energistically enhance high
-    standards in models and professional expertise. Intrinsicly iterate
-    extensible metrics for prospective opportunities. Continually develop
-    leading-edge experiences through quality e-services.
+    <p>
+      Seamlessly syndicate cutting-edge architectures rather than collaborative
+      collaboration and idea-sharing. Proactively incubate visionary interfaces
+      whereas premium benefits. Seamlessly negotiate ubiquitous leadership
+      skills rather than parallel ideas. Dramatically visualize superior
+      interfaces for best-of-breed alignments. Synergistically formulate
+      performance based users through customized relationships. Interactively
+      deliver cross-platform ROI via granular systems. Intrinsicly enhance
+      effective initiatives vis-a-vis orthogonal outsourcing. Rapidiously
+      monetize market-driven opportunities with multifunctional users.
+      Collaboratively enhance visionary opportunities through revolutionary
+      schemas. Progressively network just in time customer service without
+      real-time scenarios. Synergistically drive e-business leadership with
+      unique synergy. Compellingly seize market positioning ROI and
+      bricks-and-clicks e-markets. Proactively myocardinate timely platforms
+      through distributed ideas. Professionally optimize enabled core
+      competencies for leading-edge sources. Professionally enhance stand-alone
+      leadership with innovative synergy. Rapidiously generate backend
+      experiences vis-a-vis long-term high-impact relationships. Authoritatively
+      supply market-driven mindshare and bricks-and-clicks opportunities.
+      Holisticly create diverse innovation through adaptive communities.
+      Efficiently empower seamless meta-services with impactful opportunities.
+      Distinctively transition virtual outsourcing with focused e-tailers.
+      <br />
+      <br />
+      <span class="text2">
+        “ Monotonectally seize superior mindshare rather than efficient
+        technology. ”
+      </span>
+      <br />
+      <br />
+
+      Compellingly enhance seamless resources through competitive content.
+      Continually actualize 24/365 alignments for resource-leveling platforms.
+      Energistically enhance high standards in models and professional
+      expertise. Intrinsicly iterate extensible metrics for prospective
+      opportunities. Continually develop leading-edge experiences through
+      quality e-services. Count: {{ getCount }}
+    </p>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from "vuex";
+
 export default {
   name: "About",
+  computed: {
+    ...mapGetters(["getCount"]),
+  },
+  methods: {
+    ...mapActions(["fetchCount"]),
+  },
+  created() {
+    this.fetchCount();
+  },
 };
 </script>
 
-<style></style>
+<style>
+p {
+  font-family: Lora;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 25px;
+  /* or 179% */
+
+  color: #495057;
+}
+.text2 {
+  font-family: Lora;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 36px;
+  line-height: 46px;
+
+  color: #d4a373;
+}
+</style>
--- src/pages/EditorsPick.vue
+++ src/pages/EditorsPick.vue
@@ -1,5 +1,56 @@
 <template>
   <div>Editors Pick</div>
+  <div id="editorsPick-section">
+    <div class="row">
+      <div class="text-white col-lg-4">
+        <div class="card">
+          <img src="../assets/img/editor1.png" class="card-img" alt="..." />
+          <div class="card-img-overlay">
+            <p class="card-text">08.08.2021</p>
+
+            <h5 class="card-title">
+              Richird Norton photorealistic rendering as real photos
+            </h5>
+            <p class="card-text after">
+              Progressively incentivize cooperative systems through technically
+              sound functionalities. The credibly productivate seamless data.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="text-white col-lg-4">
+        <div class="card">
+          <img src="../assets/img/editor2.png" class="card-img" alt="..." />
+          <div class="card-img-overlay">
+            <p class="card-text">08.08.2021</p>
+            <h5 class="card-title">
+              Richird Norton photorealistic rendering as real photos
+            </h5>
+            <p class="card-text after">
+              Progressively incentivize cooperative systems through technically
+              sound functionalities. The credibly productivate seamless data.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="text-white col-lg-4">
+        <div class="card">
+          <img src="../assets/img/editor3.png" class="card-img" alt="..." />
+          <div class="card-img-overlay">
+            <p class="card-text">08.08.2021</p>
+
+            <h5 class="card-title">
+              Richird Norton photorealistic rendering as real photos
+            </h5>
+            <p class="card-text after">
+              Progressively incentivize cooperative systems through technically
+              sound functionalities. The credibly productivate seamless data.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,4 +59,40 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+#editorsPick-section .card-img-overlay {
+  margin-top: 20%;
+}
+#editorsPick-section .card-img-overlay .after {
+  font-family: Lora;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 20px;
+  /* or 167% */
+
+  color: #e5e5e5;
+}
+#editorsPick-section .card-img-overlay p {
+  font-family: Lora;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 15px;
+  display: flex;
+  align-items: center;
+
+  color: #e5e5e5;
+}
+
+#editorsPick-section .card-img-overlay h5 {
+  font-family: Lora;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 25px;
+  /* or 139% */
+
+  color: #ffffff;
+}
+</style>
